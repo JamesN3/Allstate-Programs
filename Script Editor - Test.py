4af4f48c1f375b0eeb67d2b0b73e0652ccd4f445
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-06-04 14:19:59 -0700

    Compare_List creates invalid lists of permutations

---
+++ Script Editor - Test.py
@@ -0,0 +1,48 @@
+import csv
+import openpyxl as pyxl
+
+# Generate list of addresses in FIlter street names
+wb = pyxl.load_workbook(
+    "C:/Users/Public/Documents/Jamie's Work Folder/Filter street names.xlsx",
+)
+sheet = wb.worksheets[0]
+
+row_count = sheet.max_row
+column_count = sheet.max_column
+
+compare_list = []
+
+for row in range(1, row_count):
+    for column in range(1, column_count):
+
+        cell = str(sheet.cell(row=row, column=column).value).lower()
+
+        if cell != "none":
+            if "*" in cell:
+                while "*" in cell:
+                    # Fix Logic
+                    for i in range(0, 10):
+                        copy_str = cell.replace("*", str(i), 1)
+                        compare_list.append(copy_str)
+                    cell = copy_str
+            else:
+                compare_list.append(cell)
+
+print(compare_list)
+with open(
+    "C:/Users/Public/Documents/Jamie's Work Folder/July2021 - Copy.csv", "r"
+) as csv_file:
+
+    csv_reader = csv.reader(csv_file)
+
+    with open(
+        "C:/Users/Public/Documents/Jamie's Work Folder/new_July2021.csv", "w"
+    ) as new_file:
+        csv_writer = csv.writer(new_file, delimiter=",", lineterminator="\n")
+        for line in csv_reader:
+            check = True
+            for compare_value in compare_list:
+                if compare_value == line[2].lower():
+                    check = False
+            if check == True:
+                csv_writer.writerow(line)
