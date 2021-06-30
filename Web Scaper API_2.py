20b6aaf643aa01763143e11136d5532454f218ad
James <79486681+JamesN3@users.noreply.github.com>
2021-06-30 12:34:40 -0700

    Update Web Scaper API_2.py

--- Web Scaper API_2.py
+++ Web Scaper API_2.py
@@ -4,5 +4,7 @@ import csv
 
 payload = {"ADDRESS": "1458 26th AVE NE"`}
 source = requests.get("https://gismaps.kingcounty.gov/parcelviewer2/", params=payload)
-
-source.json
+if (source.status_code 1= 200):
+  csv_writer("Error")
+else:
+  source.json
