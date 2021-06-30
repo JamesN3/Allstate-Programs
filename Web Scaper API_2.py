b31b611ad7accc6eba77304002fa03dc6e10b713
James <79486681+JamesN3@users.noreply.github.com>
2021-06-30 13:23:43 -0700

    Update Web Scaper API_2.py

--- Web Scaper API_2.py
+++ Web Scaper API_2.py
@@ -2,8 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-payload = {"ADDRESS": "1458 26th AVE NE"`}
-source = requests.get("https://gismaps.kingcounty.gov/parcelviewer2/", params=payload)
+city_name = "Issaquah"
+address = "1458 26th Ave NE " + "Issaquah"
+address.replace(" ", "%")
+index = find(address, "th")
+parameters = {"items"}
+source = requests.get(f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}", params=parameters)
 if (source.status_code 1= 200):
   csv_writer("Error")
 else:
