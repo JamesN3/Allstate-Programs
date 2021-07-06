b12cd50349cdc959cdea92b0e1caa844ddffbab4
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-07-05 17:36:01 -0700

    Update Web Scaper_API.py

--- Web Scaper_API.py
+++ Web Scaper_API.py
@@ -2,4 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-source = requests.get("https://gismaps.kingcounty.gov/parcelviewer2/")
+word = "1458 26th AVE NE"
+word = word.replace(" ", "%")
+payload = {"identifier": "OBJECTID", "add": word}
+# Replace addition signs with modulo signs
+source = requests.get(
+    "https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?",
+    params=payload,
+)
+print(source.url)
+
+# https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin=3630200160
+# "PROPNAME": {"Single Family Residence"}
