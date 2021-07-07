e54e43b0d5c70978f559f9cb4b75e9a1d2412301
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-07-07 10:59:33 -0700

    csv_writer implemented, search query needs to be refined

--- Residence_Puller_API.py
+++ Residence_Puller_API.py
@@ -3,20 +3,33 @@ import csv
 import json
 
 
-address = "910 NE High St, Issaquah, WA 98029"
+csv_name = str(input("Type in name of csv file\n"))
 
-address = address.replace(" ", "%20")
+with open(f"C:/Users/jamie/Downloads/{csv_name}.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    x = True
+    first_line = next(csv_reader)
+    with open(f"C:/Users/jamie/Downloads/updated_{csv_name}.csv", "w") as new_file:
+        csv_writer = csv.writer(new_file, delimiter=",", lineterminator="\n")
+        first_line.append("Propname")
+        csv_writer.writerow(first_line)
+        for line in csv_reader:
+            address = str(line[2])
 
-source1 = requests.get(
-    f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
-)
+            address = address.replace(" ", "%20")
 
-pin_id = source1.json()["items"][0]["PIN"]
+            source1 = requests.get(
+                f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
+            )
 
+            pin_id = source1.json()["items"][0]["PIN"]
 
-source2 = requests.get(
-    f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
-)
+            source2 = requests.get(
+                f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
+            )
 
-residence = source2.json()["items"][0]["PROPNAME"]
-print(residence)
+            residence = str(source2.json()["items"][0]["PROPNAME"])
+            print(line)
+            line.append(residence)
+            print(line)
+            csv_writer.writerow(line)
