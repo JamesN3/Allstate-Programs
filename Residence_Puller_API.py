7a81a6ed00e622e07beead8b3d5fd92750378fbe
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-07-07 15:03:26 -0700

    Goes to different json field

--- Residence_Puller_API.py
+++ Residence_Puller_API.py
@@ -6,7 +6,6 @@ csv_name = str(input("Type in name of csv file\n"))
 
 with open(f"C:/Users/jamie/Downloads/{csv_name}.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file)
-    x = True
     first_line = next(csv_reader)
     with open(f"C:/Users/jamie/Downloads/updated_{csv_name}.csv", "w") as new_file:
         csv_writer = csv.writer(new_file, delimiter=",", lineterminator="\n")
@@ -29,11 +28,10 @@ with open(f"C:/Users/jamie/Downloads/{csv_name}.csv", "r") as csv_file:
             except:
                 proper = False
             if proper:
-                residence = str(source2.json()["items"][0]["PROPNAME"])
+                residence = str(source2.json()["items"][0]["PRESENTUSE"])
                 if residence == "":
-                    residence = "UNKNOWN"
+                    residence = "Not Avaliable"
                 line.append(residence)
-                print(line)
                 csv_writer.writerow(line)
             else:
                 print(line)
