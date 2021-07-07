b5ed246499620c3ed3a5f442ae69ae398cc5e0a0
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-07-07 09:58:52 -0700

    Something works

--- Web Scaper_API.py
+++ Web Scaper_API.py
@@ -1,16 +1,23 @@
-from bs4 import BeautifulSoup
 import requests
 import csv
+import json
+
 
 word = "1458 26th AVE NE"
 word = word.replace(" ", "%")
-payload = {"identifier": "OBJECTID", "add": word}
-# Replace addition signs with modulo signs
-source = requests.get(
-    "https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?",
-    params=payload,
-)
-print(source.url)
 
+source1 = requests.get(
+    "https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add=1458%2026th%20ave%20NE"
+)
+
+pin_id = source1.json()["items"][0]["PIN"]
 # https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin=3630200160
 # "PROPNAME": {"Single Family Residence"}
+# https://devqa.io/python-parse-json/
+
+source2 = requests.get(
+    f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
+)
+
+residence = source2.json()["items"][0]["PROPNAME"]
+print(residence)
