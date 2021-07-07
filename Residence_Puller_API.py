b9eaf12af4a3a664a67fdd211d59cf1ff2cf5ee7
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-07-07 13:20:16 -0700

    It Works

--- Residence_Puller_API.py
+++ Residence_Puller_API.py
@@ -1,6 +1,5 @@
 import requests
 import csv
-import json
 
 
 csv_name = str(input("Type in name of csv file\n"))
@@ -15,21 +14,28 @@ with open(f"C:/Users/jamie/Downloads/{csv_name}.csv", "r") as csv_file:
         csv_writer.writerow(first_line)
         for line in csv_reader:
             address = str(line[2])
-
+            proper = True
+            # Needs to be updated to work with more query searches
             address = address.replace(" ", "%20")
+            try:
+                source1 = requests.get(
+                    f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
+                )
+                pin_id = source1.json()["items"][0]["PIN"]
 
-            source1 = requests.get(
-                f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
-            )
-
-            pin_id = source1.json()["items"][0]["PIN"]
-
-            source2 = requests.get(
-                f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
-            )
-
-            residence = str(source2.json()["items"][0]["PROPNAME"])
-            print(line)
-            line.append(residence)
-            print(line)
-            csv_writer.writerow(line)
+                source2 = requests.get(
+                    f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
+                )
+            except:
+                proper = False
+            if proper:
+                residence = str(source2.json()["items"][0]["PROPNAME"])
+                if residence == "":
+                    residence = "UNKNOWN"
+                line.append(residence)
+                print(line)
+                csv_writer.writerow(line)
+            else:
+                print(line)
+                line.append("Not Avaliable")
+                csv_writer.writerow(line)
