a2ad1c1a2c0e667b4e4553ebfe50260a8e9c70b1
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-07-25 18:08:37 -0700

    It works

--- Client_Updater_Alpha.py
+++ Client_Updater_Alpha.py
@@ -7,7 +7,6 @@
 import requests
 import csv
 import concurrent.futures
-import pandas as pd
 from os import path
 from bs4 import BeautifulSoup
 
@@ -30,45 +29,49 @@ while not path.exists(PATH):
         input("Insert file path\nEx: C:\\Users\\Allstate\\Downloads\\August2021.csv\n")
     )
 
-# Reminder to ensure program does not stop in executiomn
-print("Reminder: Do not open the csv file that is being read or written!")
-# Pandas dataframa reads address_list to pass to threads
-df = pd.read_csv(PATH)
-address_list = df["Address"].tolist()
 # Creates new file path for csv file that is being written
 last_index = PATH.rfind("\\")
 new_PATH = PATH[0 : last_index + 1] + "new_" + PATH[last_index + 1 :]
-
 # Main function that is called to determine the bar that should be set
-def square_call(square_bar=1800):
-    num_square_ft = square_limit(square_bar)
+def square_call(square_bar=1980, all_info=tuple()):
+    num_square_ft = square_limit(square_bar, all_info)
     if num_square_ft < 999:
-        stopper = square_bar_test(square_bar + 10)
+        stopper = square_bar_test(square_bar + 10, all_info)
         if not stopper:
             return square_bar
-        return square_call(square_bar + 10)
+        return square_call(square_bar + 10, all_info)
     else:
-        stopper = square_bar_test(square_bar - 10)
+        stopper = square_bar_test(square_bar - 10, all_info)
         if stopper:
             return square_bar - 10
-        return square_call(square_bar - 10)
+        return square_call(square_bar - 10, all_info)
 
 
 # Counts the number of clients that fit within square_bar to determine bar to set to
-def square_limit(square_bar):
+# Implement multiprocessing to speed function call
+def square_limit(square_bar, info):
     with open(PATH, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         next(csv_reader)
         num_square_ft = 0
-        for line in csv_reader:
-            if int(line[8]) <= square_bar:
-                num_square_ft += 1
+        info_index = 0
+        # Loop through tuple
+        # Then use next(csv_reader)
+        for line, info in zip(csv_reader, all_info):
+            if info[0] == True:
+                present_use = info[1].lower()
+                if (
+                    "condo" not in present_use
+                    and "apartment" not in present_use
+                    and "mobile home" not in present_use
+                ):
+                    if int(line[8]) <= square_bar:
+                        num_square_ft += 1
     return num_square_ft
 
 
-# Returns if the boolean if clients fit within limit
-def square_bar_test(square_bar):
-    if square_limit(square_bar) < 999:
+def square_bar_test(square_bar, info):
+    if square_limit(square_bar, info) < 999:
         return True
     else:
         return False
@@ -76,91 +79,95 @@ def square_bar_test(square_bar):
 
 # The message that is sent when an error occurs
 error_message = "Error — Refer to https://blue.kingcounty.com/assessor/erealproperty/ErrorDefault.htm?aspxerrorpath=/Assessor/eRealProperty/Detail.aspx"
-# Calls function to get bar to set to
-square_bar = square_call(square_bar=1800)
 
 # Function that threads use
-def add_list(address, row_val):
-    # Bar to determine which clients should access the housing square footage data
-    global square_bar
+def add_list(csv_reader):
+    # Takes line that is going to be read from
+    line = next(csv_reader)
+    # Take address and converts to search friendly form
+    # Converts spaces " " to "%20"
+    address = str(line[2])
+    address = address.replace(" ", "%20")
+    try:
+        # Takes pin_id or parcel number required to access web data and urls
+        pin_id = requests.get(
+            f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
+        ).json()["items"][0]["PIN"]
+        # Takes present_use data Ex:"Single Family(Res)" to put in csv file
+        present_use = requests.get(
+            f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
+        ).json()["items"][0]["PRESENTUSE"]
+        if present_use == "":
+            present_use = "Not Avaliable"
+        # Creates url based off of pin_id(Parcel Number)
+        # Url is not verified to avoid web visit restriction
+        url = f"https://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr={pin_id}"
+        # Signifies that process was successful to move onto access square footage data
+        passthrough = True
+    except:
+        address = address.lower()
+        # Dictionary that is circled through to see if search result can be recovered
+        abb_dict = {
+            "mt": "mountain",
+            "mount": "mt",
+            "woodinvl": "woodinville",
+            "sunbreak": "sun break",
+            "shr": "shore",
+            "cntry": "country",
+            "clb": "club",
+            "lk": "lake",
+            "shangrila": "shangri la",
+            "shoreclub": "shore club",
+        }
+        # Means process has failed thus far unless dictionary change finds search result
+        # List comprehension?
+        #
+        passthrough = False
+        # Loops through dictionary to see if dictionary result is in address
+        for abbreviation, full in abb_dict.items():
+            # Sees if results is in address and will fix search result and the computer well tell computer
+            if f"%20{abbreviation}%20" in address:
+                address = address.replace(abbreviation, full)
+                passthrough = True
+        # If dictionary element changed, the program will submit another request for data to fix field
+        if passthrough:
+            try:
+                pin_id = requests.get(
+                    f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
+                ).json()["items"][0]["PIN"]
+                present_use = requests.get(
+                    f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
+                ).json()["items"][0]["PRESENTUSE"]
+                url = f"https://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr={pin_id}"
+            except:
+                add_info = (False,)
+                return add_info
+        else:
+            add_info = (False,)
+            return add_info
+    # Appends previous information scraped
+    add_info = (passthrough, present_use, url, pin_id)
+    return add_info
 
-    # Opens new reader object for each thread
-    with open(PATH, "r") as csv_file:
-        csv_reader = csv.reader(csv_file)
-        # Iterates to the correct row to read from
-        for _ in range(row_val):
-            next(csv_reader)
-        # Takes line that is going to be read from
-        line = next(csv_reader)
-        # Take address and converts to search friendly form
-        # Converts spaces " " to "%20"
-        address = address.replace(" ", "%20")
-        try:
-            # Takes pin_id or parcel number required to access web data and urls
-            pin_id = requests.get(
-                f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
-            ).json()["items"][0]["PIN"]
-            # Takes present_use data Ex:"Single Family(Res)" to put in csv file
-            present_use = requests.get(
-                f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
-            ).json()["items"][0]["PRESENTUSE"]
-            if present_use == "":
-                present_use = "Not Avaliable"
-            # Creates url based off of pin_id(Parcel Number)
-            # Url is not verified to avoid web visit restriction
-            url = f"https://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr={pin_id}"
-            # Signifies that process was successful to move onto access square footage data
-        except:
-            address = address.lower()
-            # Dictionary that is circled through to see if search result can be recovered
-            abb_dict = {
-                "mt": "mountain",
-                "mount": "mt",
-                "woodinvl": "woodinville",
-                "sunbreak": "sun break",
-                "shr": "shore",
-                "cntry": "country",
-                "clb": "club",
-                "lk": "lake",
-                "shangrila": "shangri la",
-                "shoreclub": "shore club",
-            }
-            # Means process has failed thus far unless dictionary change finds search result
-            passthrough = False
-            # Loops through dictionary to see if dictionary result is in address
-            for abbreviation, full in abb_dict.items():
-                # Sees if results is in address and will fix search result and the computer well tell computer
-                if f"%20{abbreviation}%20" in address:
-                    address = address.replace(abbreviation, full)
-                    passthrough = True
-            # If dictionary element changed, the program will submit another request for data to fix field
-            if passthrough:
-                try:
-                    pin_id = requests.get(
-                        f"https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add={address}"
-                    ).json()["items"][0]["PIN"]
-                    present_use = requests.get(
-                        f"https://gismaps.kingcounty.gov/parcelviewer2/pvinfoquery.ashx?pin={pin_id}"
-                    ).json()["items"][0]["PRESENTUSE"]
-                    url = f"https://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr={pin_id}"
-                except:
-                    line.append(error_message)
-                    return line
-            else:
-                line.append(error_message)
-                return line
+
+def square_footage(all_info, row_val, csv_reader):
+    global square_bar
+    global error_message
+    line = next(csv_reader)
+    if all_info[0] == True:
         # Takes square_ft data from clients
-        square_ft = int(line[8])
-        if square_ft <= square_bar:
-            if (
-                "condo" not in present_use.lower()
-                and "apartment" not in present_use.lower()
-                and "mobile home" not in present_use.lower()
-            ):
+        present_use = all_info[1].lower()
+        if (
+            "condo" not in present_use
+            and "apartment" not in present_use
+            and "mobile home" not in present_use
+        ):
+            square_ft = int(line[8])
+            if square_ft <= square_bar:
                 try:
                     # Takes request for square footage
                     source = requests.get(
-                        f"https://blue.kingcounty.com/Assessor/eRealProperty/Dashboard.aspx?ParcelNbr={pin_id}"
+                        f"https://blue.kingcounty.com/Assessor/eRealProperty/Dashboard.aspx?ParcelNbr={all_info[3]}"
                     ).text
                     # Takes text element using Beautfiul Soup
                     soup = BeautifulSoup(source, "lxml")
@@ -173,7 +180,7 @@ def add_list(address, row_val):
                         if header == "total square footage":
                             new_square_ft = tr.find_all("td")[1].text
                         else:
-                            new_square_ft = "Error"
+                            new_square_ft = "-Error-"
                     except:
                         # Loops through tr tags in table
                         for value in tr:
@@ -186,25 +193,36 @@ def add_list(address, row_val):
                             except:
                                 new_square_ft = "-Error-"
                 except:
-                    # Inputs error message if previous proccesses failed
                     new_square_ft = "-Error-"
-                # Appends square footage
-                line.append(str(new_square_ft))
             else:
                 # Appends dashes to maintain csv structure
-                line.append("---")
+                line.extend(("---", all_info[1], all_info[2]))
+                return line
         else:
-            # Appends dashes to maintain csv structure
-            line.append("---")
-        # Appends previous information scraped
-        line.append(present_use)
-        line.append(url)
+            line.extend(("---", all_info[1], all_info[2]))
+            return line
+        line.extend((new_square_ft, all_info[1], all_info[2]))
+        return line
+    else:
+        line.append(error_message)
         return line
 
 
-# Creates tuple to ensure each csv row is read for the correct row being written
-num_list = tuple(range(1, len(address_list) + 1))
-# Reads csv file to copy top line of csv_file
+# Mabye don't use csv_reader passed through
+with open(PATH, "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    first_line = next(csv_reader)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Maps function ensure the threads called first are executed first
+        all_info = [line for line in executor.map(add_list, csv_reader)]
+tuple(all_info)
+
+# Calls function to get bar to set to
+square_bar = square_call(square_bar=1980, all_info=all_info)
+
+# Reminder to ensure program does not stop in executiomn
+print("Reminder: Do not open the csv file that is being read or written!")
+
 with open(PATH, "r") as csv_file:
     csv_reader = csv.reader(csv_file)
     first_line = next(csv_reader)
@@ -213,9 +231,8 @@ with open(PATH, "r") as csv_file:
         # Adds additional elements to top row of csv data
         first_line.extend(("Real Square Footage", "Present Use", "URL"))
         csv_writer.writerow(first_line)
-        # Opens threading module
         with concurrent.futures.ThreadPoolExecutor() as executor:
             # Maps function ensure the threads called first are executed first
-            for line in executor.map(add_list, address_list, num_list):
+            for line in executor.map(square_footage, all_info, csv_reader):
                 csv_writer.writerow(line)
 print("Finished!")
