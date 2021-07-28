7a2701807a0074347f7d003783892503d7233408
JamesN3 <79486681+JamesN3@users.noreply.github.com>
2021-07-27 17:16:36 -0700

    4.0 to take names and validate them

--- Client_Updater_4.py
+++ Client_Updater_4.py
@@ -7,10 +7,8 @@
 import requests
 import csv
 import concurrent.futures
-from os import path, startfile
+from os import path
 from bs4 import BeautifulSoup
-from tkinter import *
-from tkinter import filedialog
 
 # Message is to inform user about program operations
 print(
@@ -18,53 +16,6 @@ print(
     "new_{file_name}.csv"
     ""
 )
-
-# Function for opening the
-# file explorer window
-def browseFiles():
-    filename = filedialog.askopenfilename(
-        initialdir="/",
-        title="Select a File",
-        filetypes=(("Comma Seperated Values", "*.csv*"), ("all files", "*.*")),
-    )
-
-    # Change label contents
-    label_file_explorer.configure(text="File Opened: " + filename)
-
-
-# Create the root window
-window = Tk()
-
-# Set window title
-window.title("File Explorer")
-
-# Set window size
-window.geometry("500x500")
-
-# Set window background color
-window.config(background="white")
-
-# Create a File Explorer label
-label_file_explorer = Label(window, text="File Browser", width=100, height=4, fg="blue")
-
-
-button_explore = Button(window, text="Browse Files", command=browseFiles)
-
-button_exit = Button(window, text="Exit", command=exit)
-
-# Grid method is chosen for placing
-# the widgets at respective positions
-# in a table like structure by
-# specifying rows and columns
-label_file_explorer.grid(column=1, row=1)
-
-button_explore.grid(column=1, row=2)
-
-button_exit.grid(column=1, row=3)
-
-# Let the window wait for any events
-window.mainloop()
-
 
 # Takes the PATH of the csv file
 PATH = str(
