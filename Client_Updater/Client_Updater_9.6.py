5e3a491ab629376911f72fa4d16e06246343362a
James <79486681+JamesN3@users.noreply.github.com>
2022-08-17 14:27:02 -0700

    Updated Naming Convention

--- Client_Updater/Client_Updater_9.6.py
+++ Client_Updater/Client_Updater_9.6.py
@@ -2,7 +2,7 @@
 # Program built solely for the use of Allstate Hale Insurance Inc
 #
 # Program takes prexisting client data and adds the client's
-# Housing Square Footage(Potentially), Present Use, and property detail url
+# Housing Square Footage(if num_square_ft<1000), Present Use, and property detail url
 
 import requests
 import csv
